740a926330af94f940c3fda345a123ed9bf19345
karnhao <srv33605@gmail.com>
2025-03-10 13:08:42 +0700

    cancel message api

--- controllers/messageController.go
+++ controllers/messageController.go
@@ -1,15 +1,17 @@
 package controllers
+
 import (
-    "time"
 	"onez19/models"
 	"onez19/services"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func CreateMessage(ctx *fiber.Ctx) error {
 	var messageInput struct {
-		Message string `json:"message"`
-		Username string `json:"username"`
+		Message     string `json:"message"`
+		Username    string `json:"username"`
 		WorkspaceID string `json:"workspace_id"`
 	}
 
@@ -18,11 +20,10 @@ func CreateMessage(ctx *fiber.Ctx) error {
 	}
 
 	message := models.Message{
-		Message: messageInput.Message,
-		Date: time.Now().Format(time.RFC3339),
+		Message:     messageInput.Message,
+		Date:        time.Now().Format(time.RFC3339),
 		WorkspaceID: messageInput.WorkspaceID,
-		Username: messageInput.Username,
-	
+		Username:    messageInput.Username,
 	}
 
 	if err := services.CreateMessage(message); err != nil {
@@ -32,9 +33,26 @@ func CreateMessage(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusCreated).JSON(message)
 }
 
+func DeleteMessage(ctx *fiber.Ctx) error {
+	var deleteInput struct {
+		ID string `json:"id"`
+	}
+
+	if err := ctx.BodyParser(&deleteInput); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	err := services.DeleteMessage(deleteInput.ID)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to delete message"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Message deleted successfully"})
+}
+
 func GetAllMessagesByWorkspaceID(c *fiber.Ctx) error {
 	workspaceID := c.Params("workspaceId")
-	
+
 	messages, err := services.GetAllMessagesByWorkspaceID(workspaceID)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch message"})
